5ebff8d8fb1dfa00ddd0e8bf03440c9617617028
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-01-17 16:41:29 +0100

    Progress.

---
+++ TurboCollections.Test/TurboListTests.cs
@@ -0,0 +1,126 @@
+using NUnit.Framework;
+using TurboCollections;
+
+namespace TurboCollections.Test
+{
+    public class TurboListTests
+    {
+        // [SetUp]
+        // public void Setup()
+        // {
+        // }
+
+        [Test]
+        public void NewListIsEmpty()
+        {
+            var list = new TurboList<int>();
+            Assert.Zero(list.Count);
+        }
+
+        [Test, TestCase(1), TestCase(4), TestCase(42)]
+        public void AddingAnElementIncreases(int numberOfElements)
+        {
+            var list = new TurboList<int>();
+            for (int i = 0; i < numberOfElements; i++)
+                list.Add(5);
+            Assert.AreEqual(numberOfElements,list.Count);
+        }
+
+        [Test]
+        public void AddedElementGoesInCorrectIndex()
+        {
+            var list = new TurboList<int>();
+            list.Add(5);
+            list.Add(3);
+            Assert.AreEqual(3, list.Get(1));
+        }
+
+        [Test]
+        public void GetCommandReturnsCorrectItem()
+        {
+            var list = new TurboList<int>();
+            list.Add(4);
+            Assert.AreEqual(4, list.Get(0));
+        }
+
+        [Test]
+        public void ClearRemovesEverythingFromList()
+        {
+            var list = new TurboList<int>();
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.Clear();
+            Assert.Zero(list.Count);
+        }
+        
+        [Test]
+        public void RemoveAtCorrectlyRemovesItemFromIndex()
+        {
+            var list = new TurboList<int>();
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.Add(42);
+            list.RemoveAt(1);
+            Assert.AreNotEqual(42,list.Get(1));
+        }
+        
+        [Test]
+        public void RemoveAtCorrectlyResizesList()
+        {
+            var list = new TurboList<int>();
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.RemoveAt(2);
+            Assert.AreEqual(2,list.Count);
+        }
+        
+        [Test]
+        public void ContainsReturnsTrueIfItemPresent()
+        {
+            var list = new TurboList<int>();
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            Assert.True(list.Contains(4));
+        }
+        
+        [Test]
+        public void ContainsReturnsFalseIfItemNotPresent()
+        {
+            var list = new TurboList<int>();
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            Assert.False(list.Contains(42));
+        }
+        
+        [Test]
+        public void IndexOfReturnsCorrectIndex()
+        {
+            var list = new TurboList<int>();
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            Assert.AreEqual(2, list.IndexOf(5));
+        }
+        
+        [Test]
+        public void IndexOfReturnsNegativeOneIfNothingFound()
+        {
+            var list = new TurboList<int>();
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            Assert.AreEqual(-1, list.IndexOf(42));
+        }
+        
+        // [Test]
+        // public void
+        // {
+        //     var list = new TurboList<int>();
+        // }
+    }
+}
---
+++ TurboCollections/TurboList.cs
@@ -0,0 +1,147 @@
+﻿using System;
+
+namespace TurboCollections
+{
+    public class TurboList<T>
+    {
+        private T[] items;
+        public TurboList()
+        {
+            items = Array.Empty<T>();
+        }
+
+
+
+        // returns the current amount of items contained in the list.
+        public int Count => items.Length;
+
+        
+        
+        // adds one item to the end of the list.
+        public void Add(T item)
+        {
+            items = ReSize(Count + 1, items);
+            items[^1] = item;
+        }
+
+        
+        
+        // gets the item at the specified index. If the index is outside the correct range, an exception is thrown.
+        public T Get(int index)
+        {
+            if (index > Count || index < 0)
+                throw new System.Exception("Requested index was out of range of the list!");
+            
+            return items[index];
+        }
+
+        
+        
+        // removes all items from the list.
+        public void Clear()
+        {
+            items = Array.Empty<T>();
+        }
+
+        
+        
+        // removes one item from the list. If the 4th item is removed, then the 5th item becomes the 4th, the 6th becomes the 5th and so on.
+        public void RemoveAt(int index)
+        {
+            if (index > Count || index < 0)
+                throw new System.Exception("Requested index was out of range of the list!");
+
+            var result = new T[Count - 1];
+            var currentIndex = 0;
+
+            for (int i = 0; i < Count; i++)
+            {
+                if (i != index)
+                {
+                    result[currentIndex] = items[i];
+                    currentIndex++;
+                }
+            }
+
+
+            items = result;
+        }
+
+        
+        
+        // returns true, if the given item can be found in the list, else false.
+        public bool Contains(T searchItem)
+        {
+            return IndexOf(searchItem) != -1;
+        }
+        
+        
+        
+        // returns the index of the given item if it is in the list, else -1.
+        public int IndexOf(T searchItem)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (items[i].Equals(searchItem))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+        
+        // removes the specified item from the list, if it can be found.
+        public void Remove(T item)
+        {
+            if (IndexOf(item) != -1)
+            {
+                
+            }
+        }
+        
+        // // adds multiple items to this list at once.
+        // void AddRange(IEnumerable<T> items);
+        
+        // // gets the iterator for this collection. Used by IEnumerator to support foreach.
+        // IEnumerator<T>.GetEnumerator();
+
+
+        private T[] ReSize(int lenght, T[] array)
+        {
+            var result = new T[lenght];
+            for (int i = 0; i < Math.Min(Count, lenght); i++)
+            {
+                result[i] = array[i];
+            }
+
+            return result;
+
+            
+            // Marc's stuff.
+            T[] newArray = new T[Count + 1];
+            for (int i = 0; i < Count; i++)
+            {
+                newArray[i] = items[i];
+            }
+
+            items = newArray;
+        }
+
+        // private T[] ScanAndClean()
+        // {
+        //     var result = new T[items.Length];
+        //     int currentIndex = 0;
+        //     for (int i = 0; i < items.Length; i++)
+        //     {
+        //         if (items[i] != )
+        //         {
+        //             result[currentIndex] = items[i];
+        //             currentIndex++;
+        //         }
+        //     }
+        //
+        //     return ReSize(currentIndex, result);
+        // }
+    }
+}
