7751aff162710ce70e249e0f2f22d532cfd3b888
Zargn <oskar.anderson@student.forsbergsskola.se>
2022-01-19 12:35:59 +0000

    TurboQueue added together with tests.

--- TurboCollections.Test/TurboQueueTests.cs
+++ TurboCollections.Test/TurboQueueTests.cs
@@ -1,6 +1,65 @@
-namespace TurboCollections.Test;
+using System;
+using NUnit.Framework;
 
-public class TurboQueueTests
+namespace TurboCollections.Test
 {
-    
+    public class TurboQueueTests
+    {
+        [Test]
+        public void CountReturnsZeroFromEmptyQueue()
+        {
+            TurboQueue<int> queue = new TurboQueue<int>();
+            Assert.Zero(queue.Count);
+        }
+        
+        [TestCase(1), TestCase(5)]
+        public void AddingToQueueIncreasesCount(int elementsToAdd)
+        {
+            TurboQueue<int> queue = new TurboQueue<int>();
+            for (int i = 0; i < elementsToAdd; i++)
+            {
+                queue.Enqueue(i);
+            }
+            Assert.AreEqual(elementsToAdd, queue.Count);
+        }
+        
+        [Test]
+        public void PeekReturnsCorrectValue()
+        {
+            TurboQueue<int> queue = new TurboQueue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            Assert.AreEqual(1, queue.Peek());
+        }
+        
+        [Test]
+        public void DequeueReturnsCorrectItem()
+        {
+            TurboQueue<int> queue = new TurboQueue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            Assert.AreEqual(1, queue.Dequeue());
+            queue.Dequeue();
+            Assert.AreEqual(3,queue.Dequeue());
+        }
+        
+        [Test]
+        public void ClearCorrectlyEmptiesQueue()
+        {
+            TurboQueue<int> queue = new TurboQueue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            Assert.AreEqual(0, queue.Count);
+            Assert.Catch(queue.Dequeue());
+        }
+        
+        // [Test]
+        // public void 
+        // {
+        //     TurboQueue<int> queue = new TurboQueue<int>();
+        // }
+    }
 }
--- TurboCollections/TurboQueue.cs
+++ TurboCollections/TurboQueue.cs
@@ -1,6 +1,120 @@
 namespace TurboCollections;
 
-public class TurboQueue
+public class TurboQueue<T>
 {
+    private T[] items = new T[4];
     
+    public int Count
+    {
+        get;
+        private set;
+    }
+
+    /// <summary>
+    /// This holds the current offset to apply so you get the correct data when peeking or Dequeueing.
+    /// </summary>
+    private int startIndexOffset = 0;
+
+    private int memoryToPerformanceBalance;
+
+    
+
+
+    public TurboQueue(int memoryToPerformanceBalance = 0)
+    {
+        this.memoryToPerformanceBalance = memoryToPerformanceBalance;
+    }
+
+
+    // adds one item to the back of the queue.
+    public void Enqueue(T item)
+    {
+        if (Count + startIndexOffset >= items.Length) 
+            ShiftArrayToStart();
+
+        if (Count == items.Length)
+            ReSizeToFit(Count + 1);
+
+        items[Count] = item;
+        Count++;
+    }
+    
+    
+    
+    // returns the item in the front of the queue without removing it.
+    public T Peek()
+    {
+        if (Count == 0)
+            throw new System.Exception("Queue is empty! There is nothing to remove and return!");
+        
+        return items[startIndexOffset];
+    }
+    
+    
+    
+    // returns the item in the front of the queue and removes it at the same time.
+    public T Dequeue()
+    {
+        if (Count == 0)
+            throw new System.Exception("Queue is empty! There is nothing to remove and return!");
+        
+        T itemToReturn = items[startIndexOffset];
+        items[startIndexOffset] = default(T);
+        startIndexOffset++;
+        return itemToReturn;
+    }
+    
+    
+    
+    // removes all items from the queue.
+    public void Clear()
+    {
+        startIndexOffset = 0;
+        Count = 0;
+        items = Array.Empty<T>();
+    }
+    
+    
+    
+    /// <summary>
+    /// Resizes the internal array to fit the new target lenght.
+    /// </summary>
+    /// <param name="targetLenght"></param>
+    void ReSizeToFit(int targetLenght)
+    {
+        ShiftArrayToStart();
+        
+        int newLenght = items.Length;
+        while (newLenght < targetLenght)
+        {
+            newLenght *= 2;
+        }
+
+        T[] result = new T[newLenght];
+        for (int i = 0; i < Count; i++)
+        {
+            result[i] = items[i];
+        }
+
+        items = result;
+    }
+
+    /// <summary>
+    /// Shifts the internal array back to index 0.
+    /// </summary>
+    void ShiftArrayToStart()
+    {
+        for (int i = startIndexOffset; i < Count + startIndexOffset; i++)
+        {
+            items[i - startIndexOffset] = items[i];
+        }
+    }
+    
+    
+    
+
+// // --------------- optional ---------------
+// // gets the iterator for this collection. Used by IEnumerable<T>-Interface to support foreach.
+//     IEnumerator<T> IEnumerable<T>.GetEnumerator(); 
+
 }
