507423a75337319906d27f3cc447229d696325a2
Zargn <oskar.anderson@student.forsbergsskola.se>
2022-01-19 13:58:25 +0000

    TurboCollectionsSpeedTests: Added queue speed test

--- TurboCollections/TurboQueue.cs
+++ TurboCollections/TurboQueue.cs
@@ -1,126 +1,124 @@
-namespace TurboCollections;
-
-public class TurboQueue<T>
+namespace TurboCollections
 {
-    private T[] items = new T[4];
-    
-    public int Count
+    public class TurboQueue<T>
     {
-        get;
-        private set;
-    }
+        private T[] items = new T[4];
 
-    /// <summary>
-    /// This holds the current offset to apply so you get the correct data when peeking or Dequeueing.
-    /// </summary>
-    private int startIndexOffset = 0;
-    
-    
+        public int Count { get; private set; }
 
-    // adds one item to the back of the queue.
-    public void Enqueue(T item)
-    {
-        if (Count == items.Length)
-            ReSizeToFit(Count + 1);
+        /// <summary>
+        /// This holds the current offset to apply so you get the correct data when peeking or Dequeueing.
+        /// </summary>
+        private int startIndexOffset = 0;
 
-        if (Count + startIndexOffset == items.Length)
+
+
+        // adds one item to the back of the queue.
+        public void Enqueue(T item)
         {
-            items[Count + startIndexOffset - items.Length] = item;
+            if (Count == items.Length)
+                ReSizeToFit(Count + 1);
+
+            if (Count + startIndexOffset == items.Length)
+            {
+                items[Count + startIndexOffset - items.Length] = item;
+            }
+            else
+            {
+                items[Count] = item;
+            }
+
+            Count++;
         }
-        else
+
+
+
+        // returns the item in the front of the queue without removing it.
+        public T Peek()
         {
-            items[Count] = item;
+            if (Count == 0)
+                throw new System.Exception("Queue is empty! There is nothing to return!");
+
+            return items[startIndexOffset];
         }
-        Count++;
-    }
-    
-    
-    
-    // returns the item in the front of the queue without removing it.
-    public T Peek()
-    {
-        if (Count == 0)
-            throw new System.Exception("Queue is empty! There is nothing to return!");
-        
-        return items[startIndexOffset];
-    }
-    
-    
-    
-    // returns the item in the front of the queue and removes it at the same time.
-    public T Dequeue()
-    {
-        if (Count == 0)
-            throw new System.Exception("Queue is empty! There is nothing to remove and return!");
-        
-        T itemToReturn = items[startIndexOffset];
-        items[startIndexOffset] = default(T);
-        startIndexOffset++;
-        if (startIndexOffset == items.Length)
+
+
+
+        // returns the item in the front of the queue and removes it at the same time.
+        public T Dequeue()
+        {
+            if (Count == 0)
+                throw new System.Exception("Queue is empty! There is nothing to remove and return!");
+
+            T itemToReturn = items[startIndexOffset];
+            items[startIndexOffset] = default(T);
+            startIndexOffset++;
+            if (startIndexOffset == items.Length)
+                startIndexOffset = 0;
+            return itemToReturn;
+        }
+
+
+
+        // removes all items from the queue.
+        public void Clear()
+        {
             startIndexOffset = 0;
-        return itemToReturn;
-    }
-    
-    
-    
-    // removes all items from the queue.
-    public void Clear()
-    {
-        startIndexOffset = 0;
-        Count = 0;
-        items = Array.Empty<T>();
-    }
-    
-    
-    
-    /// <summary>
-    /// Resizes the internal array to fit the new target lenght.
-    /// </summary>
-    /// <param name="targetLenght"></param>
-    void ReSizeToFit(int targetLenght)
-    {
-        ShiftArrayToStart();
-        
-        int newLenght = items.Length;
-        while (newLenght < targetLenght)
-        {
-            newLenght *= 2;
+            Count = 0;
+            items = Array.Empty<T>();
         }
 
-        T[] result = new T[newLenght];
-        for (int i = 0; i < Count; i++)
+
+
+        /// <summary>
+        /// Resizes the internal array to fit the new target lenght.
+        /// </summary>
+        /// <param name="targetLenght"></param>
+        void ReSizeToFit(int targetLenght)
         {
-            result[i] = items[i];
+            ShiftArrayToStart();
+
+            int newLenght = items.Length;
+            while (newLenght < targetLenght)
+            {
+                newLenght *= 2;
+            }
+
+            T[] result = new T[newLenght];
+            for (int i = 0; i < Count; i++)
+            {
+                result[i] = items[i];
+            }
+
+            items = result;
         }
 
-        items = result;
-    }
 
-    
-    
-    /// <summary>
-    /// Shifts the internal array back to index 0.
-    /// </summary>
-    void ShiftArrayToStart()
-    {
-        T[] result = new T[items.Length];
 
-        int resetToZeroOffset = 0;
-        for (int i = 0; i < Count; i++)
+        /// <summary>
+        /// Shifts the internal array back to index 0.
+        /// </summary>
+        void ShiftArrayToStart()
         {
-            if (i + startIndexOffset == items.Length)
-                resetToZeroOffset = items.Length * -1;
-            result[i] = items[i + startIndexOffset + resetToZeroOffset];
+            T[] result = new T[items.Length];
+
+            int resetToZeroOffset = 0;
+            for (int i = 0; i < Count; i++)
+            {
+                if (i + startIndexOffset == items.Length)
+                    resetToZeroOffset = items.Length * -1;
+                result[i] = items[i + startIndexOffset + resetToZeroOffset];
+            }
+
+            items = result;
         }
 
-        items = result;
-    }
-    
-    
-    
+
+
 
 // // --------------- optional ---------------
 // // gets the iterator for this collection. Used by IEnumerable<T>-Interface to support foreach.
 //     IEnumerator<T> IEnumerable<T>.GetEnumerator(); 
 
+    }
 }
---
+++ TurboCollectionsSpeedTests/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.ComponentModel.Design;
+using System.Diagnostics;
+using TurboCollections;
+
+namespace TurboCollectionsSpeedTests
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            SpeedTests speedTests = new();
+            speedTests.Selection();
+        }
+    }
+
+    class SpeedTests
+    {
+        private const int testIterations = 100000000;
+        
+        public void Selection()
+        {
+            Console.WriteLine("(1) Run list test");
+            Console.WriteLine("(2) Run stack test");
+            Console.WriteLine("(3) Run queue test");
+            while (true)
+            {
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        ListTester();
+                        break;
+                    case "2":
+                        StackTester();
+                        break;
+                    case "3":
+                        QueueTester();
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect input");
+                        break;
+                }
+            }
+        }
+
+        void ListTester()
+        {
+            Stopwatch stopwatch = new();
+            stopwatch.Start();
+        }
+
+        void StackTester()
+        {
+            
+        }
+
+        void QueueTester()
+        {
+            Console.WriteLine("Starting queue test");
+            Console.WriteLine("Starting queue write test. Writes 100 000 000 entries to a queue.");
+
+            TurboQueue<int> queue = new();
+
+            Stopwatch stopwatch = new();
+            stopwatch.Start();
+
+            for (int i = 0; i < testIterations; i++)
+            {
+                queue.Enqueue(i);
+            }
+            
+            stopwatch.Stop();
+            Console.WriteLine($"Write test took: {stopwatch.ElapsedMilliseconds} milliSeconds");
+        }
+    }
+}
+
