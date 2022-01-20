78ec168b52b6f30fa45906851e5d9dcdc30c3458
Zargn <oskar.anderson@student.forsbergsskola.se>
2022-01-20 14:18:37 +0000

    ShuntingYardAlgorithm.Test: Added tests
    First test checks if GatherToken returns a empty queue if the input is
    invalid.
    Second test checks if GatherToken returns a queue in the correct format
    and order if input is valid.

---
+++ ShuntingYardAlgorithm.Test/ShuntingYardAlgorithmTests.cs
@@ -0,0 +1,40 @@
+using NUnit.Framework;
+
+namespace ShuntingYardAlgorithm.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void GatherTokensReturnsFalseByIncorrectInput()
+    {
+        ShuntingYardAlgorithm shuntingYardAlgorithm = new();
+        var result = shuntingYardAlgorithm.GatherTokens("Hello there i am not math");
+        Assert.AreEqual(0, result.Count);
+    }
+    
+    [Test]
+    public void GatherTokensReturnsCorrectQueue()
+    {
+        ShuntingYardAlgorithm shuntingYardAlgorithm = new();
+        var result = shuntingYardAlgorithm.GatherTokens("5+32/(4*4)");
+
+        string[] comparison = new string[] {"5", "+", "32", "/", "(", "4", "*", "4", ")"};
+        int index = 0;
+        foreach (var VARIABLE in result)
+        {
+            Assert.AreEqual(comparison[index], VARIABLE);
+            index++;
+        }
+    }
+    
+    // [Test]
+    // public void ()
+    // {
+    //     ShuntingYardAlgorithm shuntingYardAlgorithm = new();
+    // }
+}
