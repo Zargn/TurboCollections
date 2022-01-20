1be1ed76c302a3ab252850f200a989162abb0be7
Zargn <oskar.anderson@student.forsbergsskola.se>
2022-01-20 11:21:56 +0000

    ShuntingYardAlgorithm: Added empty base objects.
    - ShuntingYardAlgorithm class: Will contain the main working parts of this
      program.
    - Math enum: Holds the current calculation mode.
    - MathOperation struct: Holds two values and a calculation mode.

--- ShuntingYardAlgorithm/Program.cs
+++ ShuntingYardAlgorithm/Program.cs
@@ -1,3 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using TurboCollections;
 
-Console.WriteLine("Hello, World!");
+namespace ShuntingYardAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShuntingYardAlgorithm shuntingYardAlgorithm = new();
+
+            decimal? result = null;
+            while (result == null)
+            {
+                result = shuntingYardAlgorithm.Calculate("INPUTVALUEHERE")
+            }
+        }
+    }
+
+    public struct MathOperation
+    {
+        public MathOperation(decimal value1, decimal value2, Math calculationMode)
+        {
+            this.value1 = value1;
+            this.value2 = value2;
+            this.calculationMode = calculationMode;
+        }
+        
+        public decimal value1;
+        public decimal value2;
+        public Math calculationMode;
+    }
+
+    public enum Math
+    {
+        Add,
+        Subtract,
+        Divide,
+        Multiply
+    }
+
+    class ShuntingYardAlgorithm
+    {
+        public decimal? Calculate(string input)
+        {
+            
+            // Calculation failed so null is returned.
+            return null;
+        }
+    }
+}
+
