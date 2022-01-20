d016e4f4a74c6e841b2fdf24ce2b9eaa2e1bb5a6
Zargn <oskar.anderson@student.forsbergsskola.se>
2022-01-20 14:30:36 +0000

    ShuntingYardAlgorithm: Added xml documentation & cleanup.

--- ShuntingYardAlgorithm/Program.cs
+++ ShuntingYardAlgorithm/Program.cs
@@ -8,55 +8,79 @@ namespace ShuntingYardAlgorithm
         {
             ShuntingYardAlgorithm shuntingYardAlgorithm = new();
 
-            decimal? result = null;
-            while (result == null)
+            TurboQueue<string> result = new TurboQueue<string>();
+            while (result.Count == 0)
             {
-                result = shuntingYardAlgorithm.Calculate("INPUTVALUEHERE");
+                result = shuntingYardAlgorithm.ConvertToReversePolish("I AM NOT MATH");
             }
         }
     }
 
-    public struct MathOperation
-    {
-        public MathOperation(decimal value, Math calculationMode)
-        {
-            this.value = value;
-            this.calculationMode = calculationMode;
-        }
-        
-        public decimal value;
-        public Math calculationMode;
-    }
+    // public struct MathOperation
+    // {
+    //     public MathOperation(decimal value, Math calculationMode)
+    //     {
+    //         this.value = value;
+    //         this.calculationMode = calculationMode;
+    //     }
+    //     
+    //     public decimal value;
+    //     public Math calculationMode;
+    // }
 
-    public enum Math
-    {
-        Add,
-        Subtract,
-        Divide,
-        Multiply
-    }
+    // public enum Math
+    // {
+    //     Add,
+    //     Subtract,
+    //     Divide,
+    //     Multiply
+    // }
 
     public class ShuntingYardAlgorithm
     {
         private char[] ValidCharacters = new char[] {'+', '-', '*', '/', '(', ')'};
         
-        public decimal? Calculate(string input)
+        public TurboQueue<string> ConvertToReversePolish(string input)
         {
-            if (!EnsureInputValidity(input))
-                return null;
-                // Calculation failed so null is returned.
-            return 5;
+            var tokenQueue = GatherTokens(input);
+            if (tokenQueue.Count == 0)
+                return new TurboQueue<string>();
+
+            return ConvertFromTokenQueue(tokenQueue);
+        }
+        
+        /*
+        1.  While there are tokens to be read:
+        2.        Read a token
+        3.        If it's a number add it to queue
+        4.        If it's an operator
+        5.               While there's an operator on the top of the stack with greater precedence:
+        6.                       Pop operators from the stack onto the output queue
+        7.               Push the current operator onto the stack
+        8.        If it's a left bracket push it onto the stack
+        9.        If it's a right bracket 
+        10.            While there's not a left bracket at the top of the stack:
+        11.                     Pop operators from the stack onto the output queue.
+        12.             Pop the left bracket from the stack and discard it
+        13. While there are operators on the stack, pop them to the queue */
+
+        public TurboQueue<string> ConvertFromTokenQueue(TurboQueue<string> tokenQueue)
+        {
+            
+            
+            
+            
+            // TODO: TEMPORARY
+            return new TurboQueue<string>();
         }
 
-        private bool EnsureInputValidity(string input)
-        {
-            foreach (var character in input)
-            {
-                
-            }
-            return false;
-        }
 
+
+        /// <summary>
+        /// Converts a string into a queue of math tokens.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns>Returns a queue of tokens, or a empty queue if input is invalid.</returns>
         public TurboQueue<string> GatherTokens(string input)
         {
             TurboQueue<string> result = new();
@@ -66,10 +90,10 @@ namespace ShuntingYardAlgorithm
             {
                 if (Char.IsDigit(character))
                 {
-                    number = character + number;
+                    number += character;
                     continue;
                 }
-                else if (number != null)
+                if (number != null)
                 {
                     result.Enqueue(number);
                     number = null;
@@ -85,6 +109,13 @@ namespace ShuntingYardAlgorithm
             return result;
         }
 
+        
+        
+        /// <summary>
+        /// Checks whether the supplied character is a valid character.
+        /// </summary>
+        /// <param name="character">Character to check</param>
+        /// <returns>Boolean representing if the character supplied is valid or not.</returns>
         private bool CharIsValidCheck(char character)
         {
             foreach (var validCharacter in ValidCharacters)
