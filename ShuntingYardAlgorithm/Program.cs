9ddaf44a8665cea903b8d7215026d34a6e04c59b
Zargn <oskar.anderson@student.forsbergsskola.se>
2022-01-21 11:19:29 +0000

    ShuntingYardAlgorithm: Implemented the ReversePolishCalculator class

--- ShuntingYardAlgorithm/Program.cs
+++ ShuntingYardAlgorithm/Program.cs
@@ -1,5 +1,7 @@
 ﻿using TurboCollections;
 
+
+
 namespace ShuntingYardAlgorithm
 {
     class Program
@@ -18,32 +20,65 @@ namespace ShuntingYardAlgorithm
             }
         }
     }
+    
 
+
+    /// <summary>
+    /// Static class that contains methods of calculating a resulting decimal number from a Reverse Polish input.
+    /// </summary>
     public static class ReversePolishCalculator
     {
+        /// <summary>
+        /// Calculate the result of the Reverse Polish Notation inside the inputQueue.
+        /// </summary>
+        /// <param name="inputQueue">Sequence to use for calculation</param>
+        /// <returns>Result of calculation.</returns>
         public static decimal CalculateFromQueue(TurboQueue<Token> inputQueue)
         {
-            /*
-                While there are tokens in the input queue:
-                Dequeue a token
-                If token is a number
-                                Add it to the result stack
-                If token is a operator
-                                Pop the two top numbers in the stack and apply the operator to them.
-                                Push the result back to the stack.
-             */
+            TurboStack<decimal> result = new();
             
+            while (inputQueue.Count != 0)
+            {
+                var currentToken = inputQueue.Dequeue();
+                if (currentToken.Type == TokenType.Number)
+                {
+                    result.Push(currentToken.Value);
+                    continue;
+                }
+                
+                // The reason for the reverse order of first and second value in this method call is that the stack is
+                // from right to left, so if we want to do 3-1, that would in the stack be 1, 3. Popping the first
+                // number would return 1, and the second time 3.
+                result.Push(ApplyOperator(currentToken, result.Pop(), result.Pop()));
+            }
 
-            // TurboStack<int> result = new();
-            //
-            // while (inputQueue.Count != 0)
-            // {
-            //     var currentToken = inputQueue.Dequeue();
-            //     if (currentToken.Type == TokenType.Number)
-            //         result.Push(result);
-            // }
+            return result.Peek();
+        }
 
-            return 0;
+        
+        
+        /// <summary>
+        /// Applies the operator to the two most recent values.
+        /// </summary>
+        /// <param name="currentToken">Current token taken from queue.</param>
+        /// <param name="secondValue">Second value</param>
+        /// <param name="firstValue">First value</param>
+        /// <returns>result of operator application to the two numbers.</returns>
+        static decimal ApplyOperator(Token currentToken, decimal secondValue, decimal firstValue)
+        {
+            switch(currentToken.Type)
+            {
+                case TokenType.Add:
+                    return firstValue + secondValue;
+                case TokenType.Subtract:
+                    return firstValue - secondValue;
+                case TokenType.Divide:
+                    return firstValue / secondValue;
+                case TokenType.Multiply:
+                    return firstValue * secondValue;
+            }
+
+            throw new Exception("Token doesn't match any implemented math operator! Your options are Add, Subtract, Divide, Multiply.");
         }
     }
 }
