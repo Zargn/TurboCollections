14c07e2082ff1d0b6fee756fd7e8dbc01062dde8
Zargn <oskar.anderson@student.forsbergsskola.se>
2022-01-27 14:34:52 +0000

    TurboSort.cs: Added todo comments
    Test message

--- TurboCollections/TurboSort.cs
+++ TurboCollections/TurboSort.cs
@@ -2,6 +2,10 @@ namespace TurboCollections
 {
     public static class TurboSort
     {
+        // 1. Add a BubbleSort(TurboList<int>)-Method that sorts the List using Bubblesort.
         
+        // 2. Add a QuickSort(TurboList<int>)-Method that sorts the List using Quicksort.
+        
+        // 3. Bonus: Add a SelectionSort(TurboList<int>)-Method that sorts the List using SelectionSort.
     }
 }
